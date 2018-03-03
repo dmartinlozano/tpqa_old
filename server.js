f4aa3badf705536b0e91ac97a54d461613cbb425
Unknown <mlvasco90@gmail.com>
2018-03-03 22:40:49 +0100

    Create navbar component

--- server.js
+++ server.js
@@ -2,5 +2,8 @@ var express = require("express");
 
 var app = express();
 
+// Create link to Angular build directory
+app.use(express.static(__dirname + "/front/dist/"));
+
 // node server running =========================================================
 app.listen(8080);
