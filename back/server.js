85aafe6aa796a2314b66edb4f9d708a37fd0783a
Unknown <mlvasco90@gmail.com>
2018-03-04 10:47:51 +0100

    Back Folder Refactor

--- back/server.js
+++ back/server.js
@@ -3,7 +3,9 @@ var express = require("express");
 var app = express();
 
 // Create link to Angular build directory
-app.use(express.static(__dirname + "/front/dist/"));
+app.use(express.static(__dirname + "/../front/dist/"));
 
 // node server running =========================================================
-app.listen(8080);
+app.listen(8080, function(){
+  console.log("Running server!");
+});
